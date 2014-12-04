79b8990cfa9967170ab7f83ad302aa6992350598
tungnt620 <tungnt620@2b89a792-2fc4-a122-7e9f-98dd4f255c6b>
2014-12-04 07:38:07 +0000

    Test

--- trunk/vexekhach/src/test/java/com/vexeonline/service/khachhangservice/TestGetListInfoDanhGiaByIdNhaXe.java
+++ trunk/vexekhach/src/test/java/com/vexeonline/service/khachhangservice/TestGetListInfoDanhGiaByIdNhaXe.java
@@ -7,24 +7,31 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.vexeonline.domain.BenXe;
+import com.vexeonline.domain.ChuyenXe;
+import com.vexeonline.domain.DanhGia;
 import com.vexeonline.domain.DiaChi;
 import com.vexeonline.domain.GiaVe;
+import com.vexeonline.domain.HanhKhach;
 import com.vexeonline.domain.LichTuyen;
 import com.vexeonline.domain.NgayCuaTuan;
 import com.vexeonline.domain.NhaXe;
+import com.vexeonline.domain.TrangThaiChuyenXe;
 import com.vexeonline.domain.TuyenXe;
+import com.vexeonline.domain.VeXe;
 import com.vexeonline.domain.Xe;
+import com.vexeonline.domain.json.ThongTinDanhGia;
 import com.vexeonline.service.KhachHangService;
 import com.vexeonline.service.KhachHangServiceImpl;
 import com.vexeonline.utils.HibernateUtil;
 
-public class TestGetListChuyenXe {
+public class TestGetListInfoDanhGiaByIdNhaXe {
 	private static SessionFactory sessionFactory;
-	private KhachHangService khachHang = new KhachHangServiceImpl();
+	private KhachHangService khachHangService = new KhachHangServiceImpl();
 
 	@BeforeClass
 	public static void beforeTest() {
@@ -38,11 +45,13 @@ public class TestGetListChuyenXe {
 
 	@Test
 	public void test1() {
-		addData();
-		List<TuyenXe> list = khachHang.getListChuyenXe("Gia Lai", "HCM", Date.valueOf("2014-11-24"), 1);
-		org.junit.Assert.assertTrue(list.size() == 1);
-	}
+		addData();	
+		List<ThongTinDanhGia> list = khachHangService.getListInfoDanhGiaByNhaXe(1);
+		
+		Assert.assertTrue(list.get(0).getNgayDi().toString().equals("2014-11-24") );
 	
+	}
+
 	private void addData() {
 		Session session = sessionFactory.openSession();
 		DiaChi diaChi1 = new DiaChi();
@@ -81,7 +90,7 @@ public class TestGetListChuyenXe {
 
 		LichTuyen lichTuyen = new LichTuyen();
 		lichTuyen.setThu(NgayCuaTuan.MONDAY);
-		lichTuyen.setGioDi(Time.valueOf("17:30:00"));
+		lichTuyen.setGioDi(Time.valueOf("18:00:00"));
 		lichTuyen.setTongThoiGian(12.5);
 		lichTuyen.setXe(xe);
 		lichTuyen.setTuyenXe(tuyenXe);
@@ -100,8 +109,34 @@ public class TestGetListChuyenXe {
 		//
 		lichTuyen.getGiaVes().add(giaVe);
 
+		ChuyenXe chuyenXe = new ChuyenXe();
+		chuyenXe.setNgayDi(Date.valueOf("2014-11-24"));
+		chuyenXe.setLichTuyen(lichTuyen);
+		chuyenXe.setTrangThai(TrangThaiChuyenXe.BINHTHUONG);
+		session.save(chuyenXe);
+
+		HanhKhach hanhKhach = new HanhKhach();
+		hanhKhach.setEmail("tungnt620@gmail.com");
+		hanhKhach.setSdt("012212132");
+		hanhKhach.setTenHanhKhach("nguyen tung");
+		session.save(hanhKhach);
+		
+		VeXe veXe = new VeXe();
+		veXe.setChoNgoi(3);
+		veXe.setChuyenXe(chuyenXe);
+		veXe.setHanhKhach(hanhKhach);
+		session.save(veXe);
+		
+		DanhGia danhGia = new DanhGia();
+		danhGia.setChuyenXe(chuyenXe);
+		danhGia.setDiem(4.9f);
+		danhGia.setNoiDung("tung nguyen");
+		danhGia.setHanhKhach(hanhKhach);
+		session.save(danhGia);
+		
 		session.flush();
 		session.close();
 
 	}
+
 }
